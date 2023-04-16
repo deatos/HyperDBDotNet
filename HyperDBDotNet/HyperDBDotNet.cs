6c474ba92d5079f71e3bc5c5acfb7aecebda8c34
deatos <deatos@gmail.com>
2023-04-16 19:41:13 -0400

    Add project files.

---
+++ HyperDBDotNet/HyperDBDotNet.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+using Newtonsoft.Json;
+
+namespace HyperDBDotNet {
+    public class HyperDBDotNet {
+        private List<string> Documents { get; set; }
+        private Matrix<double> Vectors { get; set; }
+        private Func<IEnumerable<string>, Matrix<double>> EmbeddingFunction { get; set; }
+        private Func<Matrix<double>, Vector<double>, Vector<double>> SimilarityMetric { get; set; }
+
+        public HyperDBDotNet(IEnumerable<string> documents, string key, Func<IEnumerable<string>, Matrix<double>> embeddingFunction = null, string similarityMetric = "cosine") {
+            if (embeddingFunction == null) {
+                embeddingFunction = docs => GetEmbedding(docs, key);
+            }
+
+            Documents = new List<string>();
+            EmbeddingFunction = embeddingFunction;
+            Vectors = null;
+
+            AddDocuments(documents);
+
+            switch (similarityMetric.ToLower()) {
+                case "cosine":
+                    SimilarityMetric = VectorOperations.CosineSimilarity;
+                    break;
+                case "euclidean":
+                    SimilarityMetric = VectorOperations.EuclideanMetric;
+                    break;
+                case "derrida":
+                    SimilarityMetric = VectorOperations.DerridaeanSimilarity;
+                    break;
+                default:
+                    throw new ArgumentException("Similarity metric not supported. Please use either 'cosine', 'euclidean' or 'derrida'.");
+            }
+        }
+
+        public void AddDocument(string document, Vector<double> vector = null) {
+            if (vector == null) {
+                vector = EmbeddingFunction(new[] { document }).Row(0);
+            }
+
+            if (Vectors == null) {
+                Vectors = Matrix<double>.Build.Dense(0, vector.Count);
+            } else if (vector.Count != Vectors.ColumnCount) {
+                throw new ArgumentException("All vectors must have the same length.");
+            }
+
+            Vectors = Vectors.InsertRow(Vectors.RowCount, vector);
+            Documents.Add(document);
+        }
+
+        public void AddDocuments(IEnumerable<string> documents) {
+            foreach (var document in documents) {
+                AddDocument(document);
+            }
+        }
+
+        public void Load(string storageFile) {
+            using (var reader = new StreamReader(storageFile)) {
+                var json = reader.ReadToEnd();
+                var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+
+                Vectors = Matrix<double>.Build.DenseOfRowArrays(((List<List<double>>)data["vectors"]).Select(row => row.ToArray()));
+                Documents = ((List<string>)data["documents"]);
+            }
+        }
+
+        public List<string> Query(string queryText, int topK = 5) {
+            var queryVector = EmbeddingFunction(new[] { queryText }).Row(0);
+            var rankedResults = VectorOperations.HyperSvmRankingAlgorithmSort(Vectors, queryVector, topK, SimilarityMetric);
+
+            return rankedResults.Select(index => Documents[index]).ToList();
+        }
+
+        // The GetEmbedding function would go here, but it's not possible to provide a direct C# equivalent
+        // since it depends on the OpenAI API that isn't available for C#.
+        // You can create your own implementation that uses a different text embedding library or API.
+        private static Matrix<double> GetEmbedding(IEnumerable<string> documents, string key) {
+            throw new NotImplementedException("Please implement the GetEmbedding method.");
+        }
+    }
+}
---
+++ HyperDBDotNet/VectorOperations.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace HyperDBDotNet {
+    public static class VectorOperations {
+        public static Vector<double> GetNormVector(Vector<double> vector) {
+            return vector / vector.L2Norm();
+        }
+
+        public static Matrix<double> GetNormVector(Matrix<double> matrix) {
+            var normMatrix = matrix.Clone();
+            for (int i = 0; i < matrix.RowCount; i++) {
+                normMatrix.SetRow(i, GetNormVector(matrix.Row(i)));
+            }
+            return normMatrix;
+        }
+
+        public static Vector<double> CosineSimilarity(Matrix<double> vectors, Vector<double> queryVector) {
+            var normVectors = GetNormVector(vectors);
+            var normQueryVector = GetNormVector(queryVector);
+            return normVectors * normQueryVector;
+        }
+
+        public static Vector<double> EuclideanMetric(Matrix<double> vectors, Vector<double> queryVector) {
+            return EuclideanMetric(vectors, queryVector, true);
+        }
+        public static Vector<double> EuclideanMetric(Matrix<double> vectors, Vector<double> queryVector, bool getSimilarityScore = true) {
+            var rowDiffNorms = Vector<double>.Build.Dense(vectors.RowCount);
+
+            for (int i = 0; i < vectors.RowCount; i++) {
+                var rowDiff = vectors.Row(i) - queryVector;
+                rowDiffNorms[i] = rowDiff.L2Norm();
+            }
+
+            if (getSimilarityScore) {
+                rowDiffNorms = 1 / (1 + rowDiffNorms);
+            }
+
+            return rowDiffNorms;
+        }
+
+        public static Vector<double> DerridaeanSimilarity(Matrix<double> vectors, Vector<double> queryVector) {
+            var random = new Random();
+            Func<double, double> randomChange = value => value + random.NextDouble() * 0.4 - 0.2;
+
+            var similarities = CosineSimilarity(vectors, queryVector);
+            return similarities.Map(randomChange);
+        }
+
+        public static int[] HyperSvmRankingAlgorithmSort(Matrix<double> vectors, Vector<double> queryVector, int topK = 5, Func<Matrix<double>, Vector<double>, Vector<double>> metric = null) {
+            if (metric == null) {
+                metric = CosineSimilarity;
+            }
+
+            var similarities = metric(vectors, queryVector);
+            var sortedIndices = Enumerable.Range(0, similarities.Count).ToArray();
+            Array.Sort(similarities.ToArray(), sortedIndices, Comparer<double>.Create((x, y) => y.CompareTo(x)));
+            return sortedIndices.Take(topK).ToArray();
+        }
+    }
+}
