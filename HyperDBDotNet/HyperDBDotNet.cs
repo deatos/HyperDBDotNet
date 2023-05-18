afdc88a895dae31fa2e37e21b7dd0541d0fa18bf
deatos <deatos@gmail.com>
2023-05-18 12:12:34 -0400

    Converted vectoroperations to a non static class and renamed it to cpu to make room for the gpu version coming soon

--- HyperDBDotNet/HyperDBDotNet.cs
+++ HyperDBDotNet/HyperDBDotNet.cs
@@ -11,107 +11,46 @@ using Newtonsoft.Json.Linq;
 namespace HyperDBDotNet {
     public class HyperDBDotNet {
         public static Boolean DEBUGMODE = false;
-        private List<string> HDDocuments { get; set; }
-        private Matrix<double> HDVectors { get; set; }
+        private VectorOperations _vectorOperations;
+        
         
         //private Func<IEnumerable<string>, Matrix<double>> EmbeddingFunction { get; set; }
         //private Func<Matrix<double>, Vector<double>, Vector<double>> SimilarityMetric { get; set; }
         private IEmbed Embedder { get; set; }
 
         public HyperDBDotNet(IEmbed Embedder) {
-            this.HDDocuments = new List<string>();
-            this.HDVectors = null;
+            var HDDocuments = new List<string>();
+            this._vectorOperations = new VectorOperations(null, HDDocuments);
             this.Embedder = Embedder;
         }
 
-        /*
-        public HyperDBDotNet(IEnumerable<string> documents, string key, Func<IEnumerable<string>, Matrix<double>> embeddingFunction = null, string similarityMetric = "cosine") {
-            if (embeddingFunction == null) {
-                embeddingFunction = docs => GetEmbedding(docs, key);
-            }
 
-            Documents = new List<string>();
-            EmbeddingFunction = embeddingFunction;
-            Vectors = null;
-
-            AddDocuments(documents);
-
-            switch (similarityMetric.ToLower()) {
-                case "cosine":
-                    SimilarityMetric = VectorOperations.CosineSimilarity;
-                    break;
-                case "euclidean":
-                    SimilarityMetric = VectorOperations.EuclideanMetric;
-                    break;
-                case "derrida":
-                    SimilarityMetric = VectorOperations.DerridaeanSimilarity;
-                    break;
-                default:
-                    throw new ArgumentException("Similarity metric not supported. Please use either 'cosine', 'euclidean' or 'derrida'.");
-            }
-        }
-        */
         public void AddDocument(string document, Vector<double> vector = null) {
             if (document == "") return;
+            //TODO: Need better handling here
             if (vector == null) {
                // vector = EmbeddingFunction(new[] { document }).Row(0);
                 vector = this.Embedder.GetVector(document);
             }
-
-            if (HDVectors == null) {
-                HDVectors = Matrix<double>.Build.Dense(0, vector.Count);
-            } else if (vector.Count != HDVectors.ColumnCount) {
-                throw new ArgumentException("All vectors must have the same length.");
-            }
-            HDVectors = HDVectors.InsertRow(HDVectors.RowCount, vector);
-            HDDocuments.Add(document);
-        }
-
-        public void AddDocuments(IEnumerable<string> documents) {
-            foreach (var document in documents) {
-                AddDocument(document);
-            }
+            _vectorOperations.AddDocument(document, vector);
         }
 
 
         public void Save(string storageFile) {
-            var data = new Dictionary<string, object> {
-                ["vectors"] = HDVectors.ToRowArrays().ToList(),
-                ["documents"] = HDDocuments
-            };
-            var json = JsonConvert.SerializeObject(data);
-            using (var writer = new StreamWriter(storageFile)) {
-                writer.Write(json);
-            }
+            _vectorOperations.Save(storageFile);
         }
         public int Load(string storageFile) {
-            using (var reader = new StreamReader(storageFile)) {
-                var json = reader.ReadToEnd();
-                var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
-                HDDocuments = ((JArray)data["documents"]).ToObject<List<string>>();
-
-                var vectorsList = ((JArray)data["vectors"]).ToObject<List<List<double>>>();
-                var rows = vectorsList.Count;
-                var cols = vectorsList[0].Count;
-                HDVectors = Matrix<double>.Build.DenseOfRowArrays(vectorsList.Select(v => v.ToArray()).ToArray());
-                return HDVectors.RowCount;
-            }
+            return _vectorOperations.Load(storageFile);
         }
 
 
         public List<string> Query(string queryText, int topK = 5) {
             var queryVector = this.Embedder.GetVector(queryText);
             //TODO: FIX THIS
-            var rankedResults = VectorOperations.HyperSvmRankingAlgorithmSort(HDVectors, queryVector, topK);
-
-            return rankedResults.Select(index => HDDocuments[index]).ToList();
+            var rankedResults = _vectorOperations.HyperSvmRankingAlgorithmSort(queryVector, topK);
+            return _vectorOperations.GetResults(rankedResults);
         }
 
 
-    }
-    public enum SimilarityMetric {
-        CosineSimilarity,
-        EuclideanMetric,
-        DerridaeanSimilarity
     }
 }
---
+++ HyperDBDotNet/VectorOperationsCPU.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace HyperDBDotNet {
+
+    public class VectorOperationsCPU {
+        private Matrix<double> _vectors;
+        private List<string> _documents;
+
+        public VectorOperationsCPU(Matrix<double> HDVectors, List<string> HDDocuments) {
+            this._vectors = HDVectors;
+            this._documents = HDDocuments;
+        }
+        /*
+       private Vector<double> GetNormVector(Vector<double> vector) {
+           return vector / vector.L2Norm();
+       }
+
+       private Matrix<double> GetNormVector(Matrix<double> matrix) {
+           var normMatrix = Matrix<double>.Build.Dense(matrix.RowCount, matrix.ColumnCount);
+           Parallel.For(0, matrix.RowCount, i => {
+               normMatrix.SetRow(i, GetNormVector(matrix.Row(i)));
+           });
+           return normMatrix;
+       }
+       */
+        private void NormalizeInPlace(Vector<double> vector) {
+            vector /= vector.L2Norm();
+        }
+
+        private void NormalizeInPlace(Matrix<double> matrix) {
+            Parallel.For(0, matrix.RowCount, i =>
+            {
+                var row = matrix.Row(i);
+                NormalizeInPlace(row);
+                matrix.SetRow(i, row);
+            });
+        }
+      /* public static Vector<double> CosineSimilarity(Matrix<double> vectors, Vector<double> queryVector) {
+            var normVectors = GetNormVector(vectors);
+            var normQueryVector = GetNormVector(queryVector);
+            return normVectors * normQueryVector;
+        }*/
+
+        public Vector<double> CosineSimilarity(Vector<double> queryVector) {
+            NormalizeInPlace(_vectors);
+            NormalizeInPlace(queryVector);
+            return _vectors * queryVector;
+        }
+
+
+        public int[] HyperSvmRankingAlgorithmSort(Vector<double> queryVector, int topK = 5, Func<Matrix<double>, Vector<double>, Vector<double>> metric = null) {
+          //  if (metric == null) {
+            //    metric = CosineSimilarity;
+           // }
+
+            var similarities = CosineSimilarity(queryVector);
+            var sortedIndices = Enumerable.Range(0, similarities.Count).ToArray();
+            Array.Sort(similarities.ToArray(), sortedIndices, Comparer<double>.Create((x, y) => y.CompareTo(x)));
+            return sortedIndices.Take(topK).ToArray();
+        }
+
+        public void Save(string storageFile) {
+            var data = new Dictionary<string, object> {
+                ["vectors"] = _vectors.ToRowArrays().ToList(),
+                ["documents"] = _documents
+            };
+            var json = JsonConvert.SerializeObject(data);
+            using (var writer = new StreamWriter(storageFile)) {
+                writer.Write(json);
+            }
+        }
+        public int Load(string storageFile) {
+            using (var reader = new StreamReader(storageFile)) {
+                var json = reader.ReadToEnd();
+                var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                _documents = ((JArray)data["documents"]).ToObject<List<string>>();
+
+                var vectorsList = ((JArray)data["vectors"]).ToObject<List<List<double>>>();
+                var rows = vectorsList.Count;
+                var cols = vectorsList[0].Count;
+                _vectors = Matrix<double>.Build.DenseOfRowArrays(vectorsList.Select(v => v.ToArray()).ToArray());
+                return _vectors.RowCount;
+            }
+        }
+        public void AddDocument(string document, Vector<double> vector) {
+          
+
+            if (_vectors == null) {
+                _vectors = Matrix<double>.Build.Dense(0, vector.Count);
+            } else if (vector.Count != _vectors.ColumnCount) {
+                throw new ArgumentException("All vectors must have the same length.");
+            }
+            _vectors = _vectors.InsertRow(_vectors.RowCount, vector);
+            _documents.Add(document);
+        }
+        public List<string> GetResults(int[] RankedResults) {
+            return RankedResults.Select(index => _documents[index]).ToList();
+        }
+    }
+}
